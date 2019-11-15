96a0ef63493e15cf2ea532a01739f659e01b308d
1047984367@qq.com <as1314520>
2019-11-15 11:48:08 +0800

    初始化

--- tcc-transaction-tutorial-sample/tcc-transaction-http-sample/tcc-transaction-http-capital/src/main/java/org/mengyun/tcctransaction/sample/http/capital/service/CapitalAccountServiceImpl.java
+++ tcc-transaction-tutorial-sample/tcc-transaction-http-sample/tcc-transaction-http-capital/src/main/java/org/mengyun/tcctransaction/sample/http/capital/service/CapitalAccountServiceImpl.java
@@ -4,9 +4,7 @@ import org.mengyun.tcctransaction.sample.capital.domain.repository.CapitalAccoun
 import org.mengyun.tcctransaction.sample.http.capital.api.CapitalAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.remoting.httpinvoker.SimpleHttpInvokerServiceExporter;
 
-import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
 
 /**
@@ -23,15 +21,5 @@ public class CapitalAccountServiceImpl implements CapitalAccountService{
     public BigDecimal getCapitalAccountByUserId(long userId) {
         return capitalAccountRepository.findByUserId(userId).getBalanceAmount();
     }
-
-
-    @PostConstruct
-    public void ddd(){
-        SimpleHttpInvokerServiceExporter capitalAccountServiceExporter = (SimpleHttpInvokerServiceExporter) applicationContext.getBean("capitalAccountServiceExporter");
-        CapitalAccountService service = (CapitalAccountService) capitalAccountServiceExporter.getService();
-        BigDecimal capitalAccountByUserId = service.getCapitalAccountByUserId(2000);
-        System.out.println(capitalAccountByUserId);
-    }
-
 
 }
